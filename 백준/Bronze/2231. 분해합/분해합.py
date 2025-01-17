2bac9b90771b34fdc46eac09a891ab579e15b240
East-kang <rkdehdgus1212@naver.com>
2025-01-17 13:40:13 +0900

    [Bronze II] Title: 분해합, Time: 948 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,24 +1,11 @@
-N=int(input())
+n = int(input())  # 분해합을 입력값으로 받음
 
-for i in range(N//2, N):
-    a=[]
-    num=i
-    digit,sum,result=0,0,0
-
-    while num//(10**digit)>0:             # 임의의 정수 자릿수 구하기 (= 10의 digit-1 제곱)
-        digit+=1
-
-    for j in range(digit-1, -1, -1):     # 임의의 변수 자릿수에 해당하는 정수 값 저장
-        a.append(num//(10**j))
-        num=num%(10**j)
-    a.append(num)
-
-    for j in range(0, digit):
-        sum+=a[digit-j-1]*(10**j)+a[digit-j-1]
-        result+=a[digit-j-1]*(10**j)
-
-    if sum==N:
-        print(result)
+for i in range(1, n+1):   # 해당 분해합의 생성자 찾기
+    num = sum((map(int, str(i))))  # i의 각 자릿수를 더함
+    num_sum = i + num  # 분해합 = 생성자 + 각 자릿수의 합
+    # i가 작은 수부터 차례로 들어가므로 처음으로 분해합과 입력값이 같을때가 가장 작은 생성자를 가짐
+    if num_sum == n:
+        print(i)
         break
-    elif i==N-1:
+    if i == n:  # 생성자 i와 입력값이 같다는 것은 생성자가 없다는 뜻
         print(0)
