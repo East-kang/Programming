a22c8f4fd722de650ae7fad0db069c0956fc7d2c
East-kang <rkdehdgus1212@naver.com>
2025-02-21 18:00:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.24 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,6 @@
+import collections
+
+def solution(participant, completion):
+    answer = ''
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
