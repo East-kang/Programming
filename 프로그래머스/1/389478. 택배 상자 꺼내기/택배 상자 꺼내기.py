d88f4005da37b1da888752d05c131fa8344e121a
East-kang <rkdehdgus1212@naver.com>
2025-10-01 18:29:36 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,27 +1,8 @@
 def solution(n, w, num):
     answer = 0
-    rows = n//w + 1
-    cols = w
-    index = -1
-    box = []
-    
-    for row in range(rows):
-        tmp = []
-        for col in range(1, cols+1):
-            if row * w + col > n:
-                tmp.append(0)
-            else:
-                tmp.append(row * w + col)
-            
-        if row % 2 != 0:
-            tmp.reverse()
-        box.append(tmp)
-        
-    for row in range(rows):
-        for col in range(cols):
-            if box[row][col] == num:
-                index = col
-            if (box[row][col] >= num) and (col == index):
-                answer+=1
-    
+    m1 = num % (w*2)
+    m2 = (w*2-m1+1) % (w*2)
+    for i in range(num, n+1):
+        if i%(w*2) == m1 or i%(w*2) == m2:
+            answer += 1
     return answer
