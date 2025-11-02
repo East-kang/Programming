f3aaa2d13c100e07e10f2ebdfba5ead7e695dcdf
East-kang <rkdehdgus1212@naver.com>
2025-11-03 02:48:45 +0900

    [Silver II] Title: 예산, Time: 76 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,36 @@
+"""
+[이진 탐색]
+1. 예산요청 총합이 전체 국가예산을 넘는지 확인
+2. 전체 국가 예산을 N으로 나눈 값이 균등 금액
+3. 균등 금액을 넘는
+"""
+
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+        
+start = M//N
+end = M-N+1
+ans = 0
+if sum(i for i in arr) <= M:
+    print(max(arr))
+    exit(0)
+    
+while start<=end:
+    mid = (start+end)//2
+    s = 0
+    for i in arr:
+        if i<=mid:
+            s += i
+        else:
+            s += mid
+    if s > M:
+        end = mid - 1
+    else:
+        
+        start = mid + 1
+        ans = max(ans, mid)
+
+print(ans)
