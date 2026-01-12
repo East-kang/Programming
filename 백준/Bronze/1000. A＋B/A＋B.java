43a2f893ec7d0184d66e9a5f300aaed08d101233
East-kang <rkdehdgus1212@naver.com>
2026-01-13 00:30:17 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter br2 = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br1.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        br2.write(Integer.toString(A+B));
+        br2.flush();
+    }
+}
