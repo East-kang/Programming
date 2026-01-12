6e2e9368ff00a1a22ab431a207c8dc1a5132b221
East-kang <rkdehdgus1212@naver.com>
2026-01-13 00:32:50 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
