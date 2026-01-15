4207d1430bb9578c9bb56fb56a2d220fbe342689
East-kang <rkdehdgus1212@naver.com>
2026-01-15 22:51:55 +0900

    [Bronze V] Title: 구구단, Time: 144 ms, Memory: 16440 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=1; i<10; i++) {
+            bw.write(Integer.toString(N) + " * " + Integer.toString(i) + " = " + Integer.toString(N*i) + '\n');
+        }
+        bw.close();
+    }
+}
