cbe1d8e8feeec8ccaa4726c6780fa8ab991ea4cf
East-kang <rkdehdgus1212@naver.com>
2026-01-16 09:21:20 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write(Integer.toString(A+B)+'\n');
+        }
+        bw.close();
+    }
+}
