e36aaba5c1995e8490e3006935a7adeba779c546
East-kang <rkdehdgus1212@naver.com>
2026-02-17 16:38:16 +0900

    [D2] Title: 나무 높이, Time: 89 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,11 +1,10 @@
 /*
 1. 최대값 뽑기
-2. one, two 날짜수 뽑기
-3. one, two 중 최솟값 뽑아서 각자 빼고 결과에 x2 해서 더하기
-4. one==0: (two+1) + (two-1)/3
-5. two==0: one*2-1
-6. 결과에 다 더하기
-*/
+2. one, two 구하기(one: 차이%2, two: 차이/2)
+3. min 구하고, 결과에 min*2 더하기
+4. one==0: (two+1) + (two-1)/3;
+5. two==0: one*2-1;
+ */
 import java.io.*;
 import java.util.*;
 
@@ -13,16 +12,12 @@ public class Solution {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		int T = Integer.parseInt(br.readLine());
-		
 		for(int t=1; t<=T; t++) {
 			int n = Integer.parseInt(br.readLine());
 			int trees[] = new int[n];
-			int max = Integer.MIN_VALUE;
+			int max = 0;
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			for(int i=0; i<n; i++) {
 				trees[i] = Integer.parseInt(st.nextToken());
 				max = Math.max(max, trees[i]);
@@ -30,21 +25,22 @@ public class Solution {
 			
 			int one=0, two=0, result=0;
 			for(int i=0; i<n; i++) {
-				int heights = max - trees[i];
-				two+=heights/2;
-				one+=heights%2;
+				int height = max - trees[i];
+				two += height/2;
+				one += height%2;
 			}
 			
-			int min = Math.min(one, two);
+			int min=Math.min(one, two);
 			one -= min;
 			two -= min;
 			result += min*2;
 			
-			if(one==0)	result += (two+1)+(two-1)/3;
-			if(two==0)	result += one*2-1;
+			if(two==0) result += one*2-1;
+			if(one==0) result += (two+1) + (two-1)/3;
 			
-			bw.write("#" + t + " " + result + "\n");
+			System.out.println("#" + t + " " + result);
 		}
-		bw.close();
+
 	}
+
 }
