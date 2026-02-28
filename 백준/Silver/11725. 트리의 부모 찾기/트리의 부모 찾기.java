ee6b31044dbc4292ddd8200cb63130e6e942e78c
East-kang <rkdehdgus1212@naver.com>
2026-02-28 18:32:13 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 980 ms, Memory: 71332 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,52 +2,54 @@ import java.io.*;
 import java.util.*;
 
 /*
- * 1. 전부 인접 리스트로 연결.
- * 2. 1부터 dfs 돌면서 노드 배열에 부모 저장 후 출력
+ * 1. 노드 클래스 구현
+ * 	  class -> data, next;
  */
 public class Main {
+	static class Node {
+		int value;
+		List<Node> children = new ArrayList<>();
+		
+		public Node(int value) {
+			this.value = value;
+		}
+	}
+	
+	static Node nodes[];
 	static int N, parent[];
-	static List<Integer> list[];
 	static boolean visited[];
     public static void main(String[] args) throws Exception {
     		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     		N = Integer.parseInt(br.readLine());
-    		list = new ArrayList[N+1];
+    		nodes = new Node[N+1];
     		parent = new int[N+1];
+    		visited = new boolean[N+1];
+    		
     		for(int i=1; i<=N; i++)
-    			list[i] = new ArrayList<>();
+    			nodes[i] = new Node(i);
     		
     		for(int i=0; i<N-1; i++) {
     			StringTokenizer st = new StringTokenizer(br.readLine());
-    			int node = Integer.parseInt(st.nextToken());
+    			int num = Integer.parseInt(st.nextToken());
     			int next = Integer.parseInt(st.nextToken());
-    			list[node].add(next);
-    			list[next].add(node);
+    			nodes[num].children.add(nodes[next]);
+    			nodes[next].children.add(nodes[num]);
     		}
     		
-    		dfs();
+    		dfs(nodes[1]);
     		
     		for(int i=2; i<=N; i++)
     			System.out.println(parent[i]);
     }
     
-    private static void dfs() {
-    		Stack<Integer> stack = new Stack<>();
-    		visited = new boolean[N+1];
-    		
-    		stack.push(1);
-    		visited[1] = true;
-    		
-    		while(!stack.isEmpty()) {
-    			int node = stack.pop();
-    			
-    			for(int next: list[node]) {
-    				if(!visited[next]) {
-    					parent[next] = node;
-    					stack.push(next);
-    					visited[next] = true;
-    				}
-    			}
-    		}
-    }
+	private static void dfs(Node node) {
+		visited[node.value] = true;
+		
+		for(Node nxt: nodes[node.value].children)
+			if(!visited[nxt.value]) {
+				visited[nxt.value] = true;
+				parent[nxt.value] = node.value;
+				dfs(nxt);
+			}	
+	}
 }
