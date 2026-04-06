82a34c6ba3ec3f012b1fc87ae1e8455c32cf6128
East-kang <rkdehdgus1212@naver.com>
2026-04-07 08:56:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 91 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+
+	static int[] cost, plan;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+
+			// 1일, 1달, 3달, 1년 비용
+			cost = new int[4];
+			plan = new int[13];
+			// 가격 정보
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 4; i++) {
+				cost[i] = Integer.parseInt(st.nextToken());
+			}
+			// 계획 정보
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= 12; i++) {
+				int days = Integer.parseInt(st.nextToken());
+				// 하루 이용권과 한달 이용권 중 최소 비용을 저장
+				plan[i] = Math.min(plan[i - 1] + days * cost[0], plan[i - 1] + cost[1]);
+				
+				// 3달 이용권을 이용할 경우를 고려
+				if(i >= 3) plan[i] = Math.min(plan[i], plan[i - 3] + cost[2]);
+			}			
+
+			System.out.println("#" + tc + " " + Math.min(plan[12], cost[3]));
+		}
+
+	}
+
+}
