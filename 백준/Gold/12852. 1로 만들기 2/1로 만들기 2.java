5cde6df96e75e5caca4c089faf701316b632b2d5
East-kang <rkdehdgus1212@naver.com>
2026-04-09 16:36:37 +0900

    [Gold V] Title: 1로 만들기 2, Time: 212 ms, Memory: 26148 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,50 +1,41 @@
-import java.io.*;
 import java.util.*;
 public class Main {
 	static class DP {
 		int cnt;
 		List<Integer> list;
 		
-		public DP(int cnt, List<Integer> list) {
-			this.cnt = cnt;
-			this.list = list;
+		public DP(int idx) {
+			this.cnt = 0;
+			this.list = new ArrayList<>();
+			list.add(idx);
 		}
 	}
 	
 	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-		DP[] dp = new DP[x+1];
-		for(int i=1; i<=x; i++) {
-			dp[i] = new DP(0, new ArrayList<>() );
-			dp[i].list.add(i);
-			}
+		int[] dp = new int[x+1];
+		int[] prev = new int[x+1];
 		
 		for(int i=2; i<=x; i++) {
-			dp[i].cnt = dp[i-1].cnt + 1;
-			dp[i].list.clear();
-			dp[i].list.add(i);
-			dp[i].list.addAll(dp[i-1].list);
+			dp[i] = dp[i-1] + 1;
+			prev[i] = i-1;
 			
 			if(i % 2 == 0) {
-				if(dp[i].cnt > dp[i/2].cnt + 1) {
-					dp[i].cnt = dp[i/2].cnt + 1;
-					dp[i].list.clear();
-					dp[i].list.add(i);
-					dp[i].list.addAll(dp[i/2].list);
+				if(dp[i] > dp[i/2] + 1) {
+					dp[i] = dp[i/2] + 1;
+					prev[i] = i/2;
 				}
 			}
 			if(i % 3 == 0) {
-				if(dp[i].cnt > dp[i/3].cnt + 1) {
-					dp[i].cnt = dp[i/3].cnt + 1;
-					dp[i].list.clear();
-					dp[i].list.add(i);
-					dp[i].list.addAll(dp[i/3].list);
+				if(dp[i] > dp[i/3] + 1) {
+					dp[i] = dp[i/3] + 1;
+					prev[i] = i/3;
 				}
 			}
 		}
-		System.out.println(dp[x].cnt);
-		for(Integer a : dp[x].list)
-			System.out.print(a + " ");
+		System.out.println(dp[x]);
+		for(int i=x; i>=1; i = prev[i])
+			System.out.print(i + " ");
 	}
 }
