79c7e70751cfd28e3c640784ed11f0cb7768ee63
East-kang <rkdehdgus1212@naver.com>
2026-04-09 17:22:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -5,20 +5,13 @@ public class Main {
 	static int cnt;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int t=0; t<T; t++) {
-			cnt = 0;
-			calculation(Integer.parseInt(br.readLine()));
-			sb.append(cnt).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	private static void calculation(int x) {
-		if(x==0) { cnt++; return; }
-		if(x>=1) calculation(x-1);
-		if(x>=2) calculation(x-2);
-		if(x>=3) calculation(x-3);
+		int[] dp = new int[11];
+		dp[1] = 1;	dp[2] = 2;	dp[3] = 4;
+		for(int i=4; i<=10; i++)
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		
+		for(int t=0; t<T; t++)
+			System.out.println(dp[Integer.parseInt(br.readLine())]);
 	}
 }
